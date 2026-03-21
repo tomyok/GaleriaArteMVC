12557adf6a3042b2a4b4cdb45a29b1f69e168767
Tomas Caceres <tomas.caceres2@alumnos.frgp.utn.edu.ar>
2026-03-21 12:54:55 -0300

    CRUD Exposiciones, Data Annotations para fecha y nombres.

---
+++ Controllers/ExposicionesController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GaleriaArte.Data;
+using GaleriaArte.Models;
+
+namespace GaleriaArte.Controllers
+{
+    public class ExposicionesController : Controller
+    {
+        private readonly GaleriaDbContext _context;
+
+        public ExposicionesController(GaleriaDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Exposiciones
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Exposiciones.ToListAsync());
+        }
+
+        // GET: Exposiciones/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var exposicion = await _context.Exposiciones
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (exposicion == null)
+            {
+                return NotFound();
+            }
+
+            return View(exposicion);
+        }
+
+        // GET: Exposiciones/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Exposiciones/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre,FechaInicio,FechaFin")] Exposicion exposicion)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(exposicion);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(exposicion);
+        }
+
+        // GET: Exposiciones/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var exposicion = await _context.Exposiciones.FindAsync(id);
+            if (exposicion == null)
+            {
+                return NotFound();
+            }
+            return View(exposicion);
+        }
+
+        // POST: Exposiciones/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,FechaInicio,FechaFin")] Exposicion exposicion)
+        {
+            if (id != exposicion.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(exposicion);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ExposicionExists(exposicion.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(exposicion);
+        }
+
+        // GET: Exposiciones/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var exposicion = await _context.Exposiciones
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (exposicion == null)
+            {
+                return NotFound();
+            }
+
+            return View(exposicion);
+        }
+
+        // POST: Exposiciones/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var exposicion = await _context.Exposiciones.FindAsync(id);
+            if (exposicion != null)
+            {
+                _context.Exposiciones.Remove(exposicion);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ExposicionExists(int id)
+        {
+            return _context.Exposiciones.Any(e => e.Id == id);
+        }
+    }
+}
--- Models/Exposicion.cs
+++ Models/Exposicion.cs
@@ -1,11 +1,17 @@
-﻿namespace GaleriaArte.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GaleriaArte.Models
 {
     public class Exposicion
     {
         public int Id{ get; set; }
 
         public string Nombre { get; set; }
+        [Display(Name ="Fecha Inicio")]
+        [DataType(DataType.Date)]
         public DateTime FechaInicio { get; set; }
+        [Display(Name = "Fecha Fin")]
+        [DataType(DataType.Date)]
         public DateTime FechaFin { get; set; }
         public List<Obra>? ObrasExpuestas { get; set; }
     }
